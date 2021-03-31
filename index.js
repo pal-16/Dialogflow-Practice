c51d1078bd7c2f9982f77a23e07e65b472086dc0
pal-16 <mantrypalak@gmail.com>
2021-04-01 01:20:33 +0530

    added procfile

--- index.js
+++ index.js
@@ -103,4 +103,4 @@ app.post('/', express.json(), (req,res)=>{
    
 });
 
-app.listen(8000, ()=>console.log("Server is live at port 8000"));
+app.listen(process.env.PORT || 8000, ()=>console.log("Server is live at port 8000"));
