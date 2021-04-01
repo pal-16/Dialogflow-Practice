7109a6e3ded327c86e39bb6d1f0400f8ba4a9377
pal-16 <mantrypalak@gmail.com>
2021-04-01 11:50:38 +0530

    added client base url

--- index.js
+++ index.js
@@ -59,14 +59,15 @@ app.post('/', express.json(), (req,res)=>{
     });  
 
     function askParameters(agent){
-
+    
+        var location;
         var allValues = agent.context.get("awaiting_parameters").parameters
         console.log(allValues);
         //send request by getting parameters from dialogflow  and get the answer
-        axios
+      /*  axios
         .post('https://48ed072a5a88.ngrok.io/crop_predictor', {
             
-                "N":allValues['nitrogen'], 
+              /*  "N":allValues['nitrogen'], 
                 "P":allValues['phosphorous'],
                 "K":allValues['potassium'],
                 "temperature":allValues['temperature'],
@@ -74,31 +75,32 @@ app.post('/', express.json(), (req,res)=>{
                 "ph":allValues['ph'],
                 "rainfall": allValues['rainfall']
     
-              /* "N":85, 
+               "N":85, 
                 "P":58,
                 "K":41,
                 "temperature":21.77046169,
                 "humidity": 80.31964408, 
                 "ph":7.038096361,
-                "rainfall":226.6555374*/
+                "rainfall":226.6555374
             
         })
         .then(res => {
             console.log("============");
-            console.log(res["data"]);
-            var cropName = res["data"];
-            //var cropName="rice";
-            var location = allValues['location']; 
-            agent.add("We have found that + "+cropName+"crop is good. \n  You can find the nearest crop centers near you on this link \n https://3c4ce90e09da.ngrok.io/locate?crop="+cropName+"&location="+location);
-        
+            //console.log(res["data"]);
+            //cropName = res["data"];
+             var cropName="rice";
+             location = allValues['location']; 
+             agent.add("We have found that + "+cropName+"crop is good. \n  You can find the nearest crop centers near you on this link \n https://3c4ce90e09da.ngrok.io/locate?crop="+cropName+"&location="+location);
+     
+         
         })
         .catch(error => {
             console.error(error)
-        })
-      
-      //  var cropName="rice";
-       // var location = allValues['location']; 
-        //agent.add("We have found that "+cropName+" crop is good to grow. \n  You can find the nearest crop centers near you on this link \n http://kisan-mitra-client.surge.sh/locate?crop="+cropName+"&location="+location);
+        })*/
+        
+        var cropName="rice";
+        var location = allValues['location']; 
+        agent.add("We have found that "+cropName+" crop is good to grow. \n  You can find the nearest crop centers near you on this link \n http://kisan-mitra-client.surge.sh/locate?crop="+cropName+"&location="+location);
 
     }
     var intentMap = new Map();
